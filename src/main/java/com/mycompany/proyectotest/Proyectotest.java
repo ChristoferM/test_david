e66255beeca5758e05bac71700165149fb99526f
christofer <cmartinezb@correo.usbcali.edu.co>
2023-07-18 12:58:55 -0500

    primero

---
+++ src/main/java/com/mycompany/proyectotest/Proyectotest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectotest;
+
+/**
+ *
+ * @author krist
+ */
+public class Proyectotest {
+
+    /*public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }*/
+}
